578f3e526a01f75205e3f99fb4a4d3abd8213b7f
Matthieu rochet <rochetmatthieu@gmail.com>
2015-12-08 12:20:40 +0100

    add model User

---
+++ app/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+    protected $primaryKey ='id_user';
+    public $timestamps = false;
+}
