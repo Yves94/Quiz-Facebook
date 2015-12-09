e8b89a5c2ad9b1a6bc1c94c490efe3010fd2d541
MikeSony <seror_m14@myges.fr>
2015-12-09 11:16:53 +0100

    add:callback

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
     'facebook' => [
     'client_id' => '1015909901808634',
     'client_secret' => '31702599f5c83eb2bc72d902dd4923b9',
-    'redirect' => 'http://localhost/Quizfb/public/',
+    'redirect' => 'http://qiz.dev/callback',
 ],
 
 ];
