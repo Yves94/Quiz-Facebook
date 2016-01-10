0232b608eed968d0b2ae5775c03f02934601c512
Matthieu rochet <rochetmatthieu@gmail.com>
2016-01-10 16:02:10 +0100

    test create Role

--- app/Role.php
+++ app/Role.php
@@ -6,52 +6,10 @@ use Zizaco\Entrust\EntrustRole;
 
 class Role extends EntrustRole
 {
-    public $id;
     private $name;
     private $display_name;
     private $description;
-    private $created_at;
-    private $updated_at;
-
-    /**
-     * @return mixed
-     */
-    public function getUpdatedAt()
-    {
-        return $this->updated_at;
-    }
-
-    /**
-     * @param mixed $updated_at
-     */
-    public function setUpdatedAt($updated_at)
-    {
-        $this->updated_at = $updated_at;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCreatedAt()
-    {
-        return $this->created_at;
-    }
-
-    /**
-     * @param mixed $created_at
-     */
-    public function setCreatedAt($created_at)
-    {
-        $this->created_at = $created_at;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+    
     /**
      * @return mixed
      */
