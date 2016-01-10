fbb1be66951b5297cef2e5000860d75d7ac6a296
Matthieu rochet <rochetmatthieu@gmail.com>
2016-01-10 15:58:44 +0100

    test create Role

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,8 +29,8 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'role' => Zizaco\Entrust\Middleware\EntrustRole::class,
-        'permission' => Zizaco\Entrust\Middleware\EntrustPermission::class,
-        'ability' => Zizaco\Entrust\Middleware\EntrustAbility::class
+        'role' => Zizaco\entrust\src\Entrust\Middleware\EntrustRole::class,
+        'permission' => Zizaco\entrust\src\Entrust\Middleware\EntrustPermission::class,
+        'ability' => Zizaco\entrust\src\Entrust\Middleware\EntrustAbility::class
     ];
 }
