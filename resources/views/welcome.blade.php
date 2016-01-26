4e070451f14aad3d14d55697d8826b3e17e88a2d
Matthieu rochet <Matthieu rochet>
2016-01-26 21:49:27 +0100

    add except verification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
         <style>
             html, body {
                 height: 100%;
@@ -36,7 +36,6 @@
         </style>
     </head>
     <body>
-        {{ csrf_field() }}
         <div class="container">
             <div class="content">
                 <div class="title">Laravel</div>
