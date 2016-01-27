55d074aa7f2e73e2fba45481a6786de7f4684c93
Matthieu rochet <Matthieu rochet>
2016-01-27 21:58:31 +0100

    test tokent view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,10 +46,10 @@
         </div>
     </body>
     <script>
+    $(function () {
     $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
+        headers: { 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content') }
+    });
+});
     </script>
 </html>
