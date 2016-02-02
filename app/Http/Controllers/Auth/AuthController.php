0f6d3c2788d7015af790d7d41e57bb908e877dcb
Matthieu rochet <rochetmatthieu@gmail.com>
2016-02-02 15:39:21 +0100

    modification routing login facebook

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -88,6 +88,6 @@ class AuthController extends Controller
         ]);*/
 
         //Auth::login($current_user,true);//Log le user
-        return Redirect::route('login');
+        return Redirect::route('/');
     }
 }
