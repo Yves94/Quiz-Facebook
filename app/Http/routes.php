db6a7f0880e153ba64950b3055cb8db2ae54adc6
Matthieu rochet <rochetmatthieu@gmail.com>
2016-02-02 15:02:15 +0100

    modification routing login facebook

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -42,7 +42,7 @@ Route::match(array('GET', 'POST'),'/', function(SammyK\LaravelFacebookSdk\Larave
     }
 
     // $token will be null if the user hasn't authenticated your app yet
-    if (!$token) {
+    if ($token == null ) {
         $helper = $fb->getRedirectLoginHelper();
         $permissions = ['email']; // optional
         $callback = 'https://quizfb.herokuapp.com/callback';
