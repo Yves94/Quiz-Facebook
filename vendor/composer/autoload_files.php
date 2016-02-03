eefa26726490c8afa977f347d73403f6cb4c674a
Matthieu rochet <rochetmatthieu@gmail.com>
2016-02-03 16:36:05 +0100

    merge all branch

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -13,8 +13,8 @@ return array(
     $vendorDir . '/danielstjules/stringy/src/Create.php',
     $vendorDir . '/psy/psysh/src/Psy/functions.php',
     $vendorDir . '/paragonie/random_compat/lib/random.php',
-    $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
-    $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
     $vendorDir . '/laravelcollective/html/src/helpers.php',
     $vendorDir . '/barryvdh/laravel-debugbar/src/helpers.php',
+    $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
+    $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
 );
