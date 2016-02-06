b72409bcf9e5947c83411c373551b09b78a400a6
Yves Legris <ylegris94@gmail.com>
2016-02-06 23:09:55 +0100

    FrontQuiz Update: Amélioration interface question

--- resources/views/FrontOffice/questionsQuiz.blade.php
+++ resources/views/FrontOffice/questionsQuiz.blade.php
@@ -5,11 +5,23 @@
 @section('content')
 
 <div class="row questionHeader">
-	<div class="col-md-6">Question X sur X</div>
-	<div class="col-md-6 text-right">4 <span class="glyphicon glyphicon-time"></span></div>
+	<div class="col-xs-4"><span class="glyphicon glyphicon-question-sign"></span>&nbsp;2 / 8</div>
+	<div class="col-xs-4 text-center">
+		<div class="joker"><span class="glyphicon glyphicon-star"></span></div>
+		<div class="joker"><span class="glyphicon glyphicon-star"></span></div>
+		<div class="joker"><span class="glyphicon glyphicon-star"></span></div>
+	</div>
+	<div class="col-xs-4 text-right">4 <span class="glyphicon glyphicon-time"></span></div>
 </div>
-<div class="row jumbotron">
-	<div class="col-md-6 col-md-offset-3 text-center questionContent">En quel année je serai ?</div>
+
+<div class="progress questionProgressTime">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+    <span class="sr-only">60% Complete</span>
+  </div>
+</div>
+
+<div class="row jumbotron questionContainer">
+	<div class="col-md-10 col-md-offset-1 text-center questionContent">You can use inline-flex as well which works pretty good and may be a little cleaner than modifying every row element with CSS.?</div>
 </div>
 
 <div class="list-group answersContent">
